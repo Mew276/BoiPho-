2faa78e3e906b8e12cc8dedf91bcf4328f7dd6dd
Mew276 <vuhopwork276@gmail.com>
2026-04-17 02:20:40 +0700

    Random spawning map

---
+++ Assets/Script/MapSpawn.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MapSpawn : MonoBehaviour
+{
+    public GameObject[] tiles;
+    public float zSpawn = 0;
+    public float tileLength = 25;
+    public int numberOfTiles = 5;
+    public Transform playerTransform;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            SpawnTile(i == 0 ? 0 : Random.Range(0, tiles.Length));
+        }
+    }
+
+    void Update()
+    {
+        if (playerTransform.position.z - 30 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tiles.Length));
+            DeleteTile();
+        }
+    }
+
+    void SpawnTile(int tileIndex)
+    {
+        GameObject go = Instantiate(tiles[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
