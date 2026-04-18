dfadc66f6f7a5429b8f014c8b037da40c460aa32
Mew276 <vuhopwork276@gmail.com>
2026-04-19 03:41:26 +0700

    no

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,33 +1,55 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
     public static bool gameStart = false;
     public static bool gameOver = false;
+
     public TextMeshProUGUI GameOverText;
     public GameObject StartingText;
     public TextMeshProUGUI CoinsText;
+
     public static int NumbersOfCoin = 0;
+
+    public static bool reStart = false;
+
     void Start()
     {
         gameStart = false;
         gameOver = false;
+        reStart = false;
         NumbersOfCoin = 0;
+
         GameOverText.gameObject.SetActive(false);
     }
+
     void Update()
     {
         CoinsText.text = "Coins : " + NumbersOfCoin;
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        // Start game
+        if (Input.GetKeyDown(KeyCode.Space) && !gameStart)
         {
             gameStart = true;
             Destroy(StartingText);
         }
 
+        // Game over
         if (gameOver)
         {
             GameOverText.gameObject.SetActive(true);
+            RestartGame();
+        }
+    }
+
+    void RestartGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
